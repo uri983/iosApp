6b2202ac0c0fef8dbfa1cc801069e1fc752513bf
uriel <velasquez_uriel@hotmail.com>
2019-03-08 15:39:33 -0500

    scaN

--- platforms/android/assets/www/js/my-app.js
+++ platforms/android/assets/www/js/my-app.js
@@ -48,39 +48,10 @@ function scan(){
 
     //SpinnerPlugin.activityStart("Iniciando Sesión...");
 
-    window.plugins.GMVBarcodeScanner.scan({}, function(err, result) { 
-    
-    //Handle Errors
-    if(err) return;
-    
-    //Do something with the data.
-    alert(result);
-    
-    });
+    mwbScanner.startScanning(function(result){
+       alert('este es el resulto' + result);
+    }, 200, 200, 300, 500);
 
-    /*cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          myApp.alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      },
-      function (error) {
-          myApp.alert("Scanning failed: " + error);
-      },
-      {
-          preferFrontCamera : true, // iOS and Android
-          showFlipCameraButton : true, // iOS and Android
-          showTorchButton : true, // iOS and Android
-          torchOn: true, // Android, launch with the torch switched on (if available)
-          saveHistory: true, // Android, save scan history (default false)
-          prompt : "Place a barcode inside the scan area", // Android
-          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-          disableAnimations : true, // iOS
-          disableSuccessBeep: false // iOS and Android
-      }
-   );*/
+    
     
 }
