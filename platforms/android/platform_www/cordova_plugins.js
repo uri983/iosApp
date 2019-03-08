4b8ed00b48873161bfb8b7bb2339eae527231219
uriel <velasquez_uriel@hotmail.com>
2019-03-08 13:49:01 -0500

    pruebita

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,55 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-dialogs.notification",
+    "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification_android",
+    "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-spinner.SpinnerPlugin",
+    "file": "plugins/cordova-plugin-spinner/www/spinner-plugin.js",
+    "pluginId": "cordova-plugin-spinner",
+    "clobbers": [
+      "SpinnerPlugin"
+    ]
+  },
+  {
+    "id": "cordova-plugin-statusbar.statusbar",
+    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "clobbers": [
+      "window.StatusBar"
+    ]
+  },
+  {
+    "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+    "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+    "pluginId": "phonegap-plugin-barcodescanner",
+    "clobbers": [
+      "cordova.plugins.barcodeScanner"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-console": "1.0.7",
+  "cordova-plugin-dialogs": "2.0.1",
+  "cordova-plugin-spinner": "1.1.0",
+  "cordova-plugin-statusbar": "1.0.1",
+  "cordova-plugin-whitelist": "1.2.2",
+  "phonegap-plugin-barcodescanner": "6.0.8"
+};
+// BOTTOM OF METADATA
+});
